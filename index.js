d0a6ab70acdc7988428c8db817f06c69f26090a7
adrianlfns <adrianlfns@yahoo.com>
2024-03-27 23:04:32 -0400

    added basic chat with history

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 
+//see https://js.langchain.com/docs/use_cases/chatbots/quickstart
 import { HuggingFaceInference } from "@langchain/community/llms/hf";
+import { HumanMessage, AIMessage  } from "@langchain/core/messages";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
+import { ChatMessageHistory } from "langchain/stores/message/in_memory";
+
+const msgHistory = new ChatMessageHistory(); 
 
 
-const model = new HuggingFaceInference({
-  temperature:0.8,
+//Model definition
+const model = new HuggingFaceInference({ 
+  //temperature: 1,
   model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+//  model: "meta-llama/Llama-2-70b-chat-hf",
+ // model: "openchat/openchat-3.5-0106",
+  //model: "codellama/CodeLlama-70b-Instruct-hf", 
   apiKey: process.env.HUGGINFACE__API_KEY
 });
 
-const res = await model.invoke("Can you list 5 business names for a a construction company?");
+
+//promp template
+const prompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    "You are a helpful assistant that can help entrepreneurs to establish a new business in Miami-Dade, Florida.  . Answer all questions to the best of your ability.",
+  ],
+  new MessagesPlaceholder("messages"),
+]);
+
+
+//chain
+const chain = prompt.pipe(model);
 
 
 
-//console.log({ res });
+export async function answerQuestion(question){ 
+  await msgHistory.addMessage(new HumanMessage(question));
+  const answer = await chain.invoke({
+    messages: await msgHistory.getMessages()
+  }); 
+  await msgHistory.addMessage(new AIMessage(answer)); 
+  return answer;
+};
+ 
 
 
 /*
---
+++ indexUI.js
@@ -0,0 +1,122 @@
+import {answerQuestion} from './index.js'
+
+
+
+var oQuestionText = document.getElementById("txtQuestion");
+var oQuestionBtn = document.getElementById("cmdQuestionSubmit");
+var oChatList = document.getElementById("chatList");
+   
+function ScrollToBottom(){
+  const scrollingElement = (document.scrollingElement || document.body);
+  scrollingElement.scrollTop = scrollingElement.scrollHeight; 
+}
+
+function SetSubmitQuestionDisabled(){ 
+  oQuestionText.setAttribute("disabled", true);
+  oQuestionText.setAttribute("editable", false); 
+  oQuestionBtn.setAttribute("disabled", true);
+}
+
+function SetSubmitQuestionEnabled(){ 
+  oQuestionText.removeAttribute("disabled");
+  oQuestionText.setAttribute("editable", true);
+  oQuestionBtn.removeAttribute("disabled");
+}
+
+function ShowLoader(){
+  var oLoaderIndicatro =  document.getElementById("loaderIndicator");
+  oLoaderIndicatro.style.display = "";
+}
+
+function HideLoader(){
+  var oLoaderIndicatro =  document.getElementById("loaderIndicator");
+  oLoaderIndicatro.style.display= "none";
+}
+
+function AppendBotMessage(strAnwwer){
+
+  var botHtmlMessage = document.createElement("li");
+  botHtmlMessage.className = "mb-2 text-primary";
+    var strMessage = `<div class="card"> 
+        <div class="card-body">
+          <p class="fw-bold mb-0 text-primary">FL Guru Bot</p> 
+          <p class="mb-0 text-primary">
+            ${strAnwwer}
+          </p>
+        </div>
+      </div>`;     
+      botHtmlMessage.innerHTML =strMessage;
+      oChatList.append(botHtmlMessage);
+}
+
+function AppendHumanMessage(strQuestion) {
+  var humanHtmlMessage = document.createElement("li");
+    humanHtmlMessage.className = "mb-2";
+    var strMessage = `<div class="card"> 
+        <div class="card-body">
+          <p class="fw-bold mb-0">You</p> 
+          <p class="mb-0">
+            ${strQuestion}
+          </p>
+        </div>
+      </div>`;     
+      humanHtmlMessage.innerHTML =strMessage;
+      oChatList.append(humanHtmlMessage);
+}
+
+async function SubmitQuery(){
+  try{ 
+    var strQuestion = oQuestionText.value;
+    if(strQuestion == "") return;
+    SetSubmitQuestionDisabled();
+    ShowLoader();                  
+    AppendHumanMessage(strQuestion);
+    ScrollToBottom(); 
+    var strAnswer = await answerQuestion(strQuestion);              
+    AppendBotMessage(strAnswer);
+    ScrollToBottom();
+
+    
+  }catch (error) {
+    console.error(error); 
+  }
+  finally
+  {
+    oQuestionText.value = "";
+    SetSubmitQuestionEnabled();
+    HideLoader();
+    oQuestionText.focus();
+  }               
+}
+
+oQuestionText.addEventListener("keyup", function(e){
+  //validates that there is text 
+  if (oQuestionText.value.trim() == "")
+  {
+    oQuestionBtn.setAttribute("disabled", true);
+  }
+  else
+  {
+    oQuestionBtn.removeAttribute("disabled");
+  }                
+});
+
+oQuestionText.addEventListener("keypress", function(e){
+  if (oQuestionText.value == "")
+  {
+    oQuestionBtn.setAttribute("disabled", true);
+  }
+  else
+  {
+    oQuestionBtn.removeAttribute("disabled");
+  }
+  if(e.key === 'Enter'){
+     e.preventDefault();  
+     SubmitQuery();  
+  }
+});
+
+oQuestionBtn.addEventListener("click",function(e){
+  SubmitQuery();
+})
+
